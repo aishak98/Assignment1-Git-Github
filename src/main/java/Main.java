5697e34794649319afc1e33ce42072ed6a6a0c16
aishak98 <akhan98@asu.edu>
2026-01-28 14:25:24 -0700

    dev: tweak welcome message for rebase conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Welcome to the Number Guessing Game! (Feature B)  ===");
+        System.out.println("=== Welcome to the Number Guessing Game! (DEV)  ===");
         // Version 1.1 - Added quit functionality
         System.out.println("Good luck!");
 
